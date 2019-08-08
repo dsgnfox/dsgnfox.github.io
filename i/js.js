1b137b1492181c7ef1d138f1910448f770ba1caf
dsgnfox <dsgnfox@gmail.com>
2019-08-08 16:51:15 +0300

    fix preload in portfolio

--- i/js.js
+++ i/js.js
@@ -3,7 +3,10 @@ document.body.onload = function() {
 	setTimeout(function() {
 		var preloader = document.getElementById('preloader');
 		if (!preloader.classList.contains('done')) {
-			preloader.classList.add('done'); 
+			preloader.classList.add('done');
+			setTimeout(function() {
+				preloader.classList.add('hide');
+			}, 400);
 		}
 	}, 500);
 }
